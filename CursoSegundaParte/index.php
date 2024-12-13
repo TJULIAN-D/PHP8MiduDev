d9079b8f152e60f1adb587e53b5a8c36641c7950
TJULIAN-D <juliandavidbonillatorres@gmail.com>
2024-12-13 09:11:09 -0500

    Functions in the same file

--- CursoSegundaParte/index.php
+++ CursoSegundaParte/index.php
@@ -1,8 +1,9 @@
 <!-- Consultando una api -->
 <?php
 
+// declare(strict_types=1);
 //Esto es a nivel de archivo Para que los tipos de ese archivo sean strictos
-declare(strict_types=1);
+
 const API_URL = "https://whenisthenextmcufilm.com/api";
 
 function get_data(string $url)
@@ -15,14 +16,23 @@ function get_data(string $url)
   return $data;
 }
 
-function get_until_message (int $days): string{
-  
-}
+function get_until_message(int $days): string
+{
 
-$data = get_data(API_URL)
+  return match (true) {
+    $days === 0 => "Hoy se estrena",
+    $days === 1 => "Mañana se Estrena",
+    $days < 7 => "Esta semana se Estrena",
+    $days < 30 => "este mes se estrena",
+    default => "$days Dias hasta el estreno",
+  };
+};
+
+$data = get_data(API_URL);
+$untilMessage = get_until_message($data["days_until"]);
 
 
-
+echo $data['days_until']
 ?>
 
 
@@ -45,7 +55,7 @@ $data = get_data(API_URL)
     <pre style="height: 300px; width: 800px; overflow: scroll;"><?= var_dump($data); ?> </pre>
     <div style="text-align: center;">
       <img src="<?= $data['poster_url'] ?>" alt="" style="height: 400px; border-radius: 25px ;">
-      <h1> <?= $data['title'] ?> </h1>
+      <h1> <?= $data['title'] . " - " . $untilMessage ?> </h1>
       <div> Fecha : <?= $data['release_date'] ?></div>
       <div> Siguiente Pelicula : <?= $data['following_production']['title'] ?></div>
     </div>
